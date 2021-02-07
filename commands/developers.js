e5206def21879c14f5bf37426ad35138c19e4d7e
SliceCraft <remco@familiedegroot.org>
2021-02-08 00:15:51 +0100

    Added developers command (please never touch)

--- commands/developers.js
+++ commands/developers.js
@@ -8,16 +8,14 @@ module.exports = {
     ],
     "description": "Show a list of devs",
     execute(message, args){
-        var commandlist = "";
-        bot.commands.forEach(command => {
-            if(command.hideCommand == false){
-                commandlist = commandlist + `${prefix}${command.name} '${command.description}'\n`;
-            }
-        })
+        var DevList = "";
+        developers.forEach(devID => {
+            DevList = DevList + `<@${devID}>\n`
+        });
         var Help = new MessageEmbed()
             .setColor("#00cc99")
-            .setTitle("Help")
-            .addField("Commands", commandlist)
+            .setTitle("Developers")
+            .setDescription(DevList)
             .setFooter(footer + " | " + ver);
         message.channel.send(Help);
     }
