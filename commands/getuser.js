20d3ea12880bc33b554a66dd22476a035b2f55d4
SliceCraft <remco@familiedegroot.org>
2021-05-07 22:18:47 +0200

    [~] Added pronouns to getuser

--- commands/getuser.js
+++ commands/getuser.js
@@ -14,12 +14,32 @@ module.exports = {
         }
         let userinfo = new MessageEmbed();
         BubblezClient.getUser(args[1]).then(user => {
+            let pronoun;
+            if(user.pronoun == "aeaer") pronoun = "Ae/Aer";
+            if(user.pronoun == "any") pronoun = "Any";
+            if(user.pronoun == "eem") pronoun = "E/Em";
+            if(user.pronoun == "faefaer") pronoun = "Fae/Faer";
+            if(user.pronoun == "hehim") pronoun = "He/Him";
+            if(user.pronoun == "heshe") pronoun = "He/She";
+            if(user.pronoun == "hethem") pronoun = "He/Them";
+            if(user.pronoun == "itits") pronoun = "It/Its";
+            if(user.pronoun == "other") pronoun = "Other";
+            if(user.pronoun == "perper") pronoun = "Per/Per";
+            if(user.pronoun == "sheher") pronoun = "She/Her";
+            if(user.pronoun == "shethem") pronoun = "She/They";
+            if(user.pronoun == "theythem") pronoun = "They/Them";
+            if(user.pronoun == "vever") pronoun = "Ve/Ver";
+            if(user.pronoun == "xexem") pronoun = "Xe/Xem";
+            if(user.pronoun == "ziehir") pronoun = "Zie/Hir";
+            if(user.pronoun == "brobro") pronoun = "BRO/Bro";
+            if(user.pronoun == "none") pronoun = "None";
             userinfo.setTitle("Bubblez Profile Info");
             userinfo.setDescription("Made with: [bubblez.js](https://www.npmjs.com/package/bubblez.js)");
             userinfo.setColor("#00cc99");
-	    userinfo.addField("UUID", user.UUID, true);
+	        userinfo.addField("UUID", user.UUID, true);
             userinfo.addField("Username", `[${user.username}](https://bubblez.app/p?${user.username})`, true);
             userinfo.addField("Displayname", user.displayname, true);
+            userinfo.addField("Pronoun", pronoun, true);
             userinfo.addField("Rank", `${user.rank ? user.rank : "user"}`, true);
             userinfo.addField("Coins", user.coins, true);
             userinfo.setFooter(ver);
